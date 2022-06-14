b777c8f971367ce5832b4046e28e94128968a981
flavien-hugs <geekactux@gmail.com>
2022-06-14 08:45:43 +0000

    edit CRUD for app

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,23 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import React, { Component, Fragment } from 'react';
 
+import store from '../store';
 import Header from './layouts/Header';
-import Dashboard from '../leads/Dashboard';
+import Dashboard from './leads/Dashboard';
+
 
 class App extends Component {
     render() {
         return (
-            <Fragment>
-                <Header />
-                <div className="container">
-                    <Dashboard />
-                </div>
-            </Fragment>
+            <Provider store={store}>
+                <Fragment>
+                    <Header />
+                    <div className="container">
+                        <Dashboard />
+                    </div>
+                </Fragment>
+            </Provider>
         );
     }
 }
