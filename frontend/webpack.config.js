2b35eb09718a2d9cc7a290e0339c933b1fbf4373
flavien-hugs <geekactux@gmail.com>
2022-06-14 06:46:53 +0000

    configure react

---
+++ frontend/webpack.config.js
@@ -0,0 +1,13 @@
+modules.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
