6f4105d860274fba8d4ce1e88c27c886b7d4d8c9
flavien-hugs <geekactux@gmail.com>
2022-06-15 23:37:48 +0000

    improve app

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,35 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import React, { Component, Fragment } from 'react';
+import { Provider as ALertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 
 import store from '../store';
 import Header from './layouts/Header';
+import Alerts from "./layouts/Alerts";
 import Dashboard from './leads/Dashboard';
+
+
+// Alert Options
+const alertOptions = {
+    timeout: 3000,
+    position: 'top center'
+}
 
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Fragment>
-                    <Header />
-                    <div className="container">
-                        <Dashboard />
-                    </div>
-                </Fragment>
+                <ALertProvider template={AlertTemplate} {...alertOptions}>
+                    <Fragment>
+                        <Header />
+                        <Alerts />
+                        <div className="container">
+                            <Dashboard />
+                        </div>
+                    </Fragment>
+                </ALertProvider>
             </Provider>
         );
     }
